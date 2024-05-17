6fb6b99c06eb8ca7c53517d44f1371a92bdec1fc
Roman Sytsykhovskyi <roman.sytsykhovskyi@gmail.com>
2024-05-17 09:35:38 +0300

    feat: creat form for user creation

--- src/app/users/create/page.tsx
+++ src/app/users/create/page.tsx
@@ -2,6 +2,7 @@
 
 import { useRouter } from "next/navigation";
 import { useState, useTransition } from "react";
+import type { FormEvent, ChangeEvent } from "react";
 
 const initUser: Partial<User> = { name: "", last_name: "" };
 
@@ -14,7 +15,64 @@ const CreateUserPage = () => {
 
   const isMutating = isFetching || isPending;
 
-  return <main></main>;
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    setIsFetching(true);
+
+    const api = await fetch("http://127.0.0.1:3500/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+
+    await api.json();
+
+    setIsFetching(false);
+
+    startTransition(() => router.push("/users"));
+  };
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    setUser((prevUser) => ({
+      ...prevUser,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <main>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={user.name}
+          onChange={handleChange}
+          disabled={isMutating}
+          placeholder="Name"
+          autoFocus
+        />
+        <input
+          type="text"
+          id="last_name"
+          name="last_name"
+          value={user.last_name}
+          onChange={handleChange}
+          disabled={isMutating}
+          placeholder="Last name"
+        />
+
+        <button type="submit" disabled={isMutating}>
+          Submit
+        </button>
+      </form>
+    </main>
+  );
 };
 
 export default CreateUserPage;
