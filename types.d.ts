1012af1ba947c6489dfad1d2cab5cd37ccf567da
Roman Sytsykhovskyi <roman.sytsykhovskyi@gmail.com>
2024-05-17 09:51:42 +0300

    prepare types for chats

--- types.d.ts
+++ types.d.ts
@@ -2,4 +2,17 @@ type User = {
   id: string;
   name: string;
   last_name: string;
+  chats: string[];
+};
+
+type Message = {
+  id: string;
+  userId: string;
+  content: string;
+};
+
+type Chat = {
+  id: string;
+  title: string;
+  messages: Message[];
 };
